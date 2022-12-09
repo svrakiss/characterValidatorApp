91739916613821df07ae5e143f52a2461b7004d7
svrakiss <59843464+svrakiss@users.noreply.github.com>
2022-12-09 19:33:04 +0000

    scuffed dynamodb config

---
+++ validator/src/main/java/com/foxy/patreon/validator/DynamoDBConfig.java
@@ -0,0 +1,46 @@
+package com.foxy.patreon.validator;
+
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+@Configuration
+@EnableDynamoDBRepositories
+  (basePackages = "com.baeldung.spring.data.dynamodb.repositories")
+public class DynamoDBConfig {
+
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB 
+          = AmazonDynamoDBClientBuilder.defaultClient();
+        //   (amazonAWSCredentials());
+        
+        if (!StringUtils.hasText(amazonDynamoDBEndpoint)) {
+            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        }
+        
+        return amazonDynamoDB;
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(
+          amazonAWSAccessKey, amazonAWSSecretKey);
+    }
+}
