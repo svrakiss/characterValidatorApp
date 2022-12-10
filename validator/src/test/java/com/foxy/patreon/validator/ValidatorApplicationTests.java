dfb49e74b2ace77db7df4cee3cb6c5cf54888b13
svrakiss <59843464+svrakiss@users.noreply.github.com>
2022-12-10 00:10:58 +0000

    properties

--- validator/src/test/java/com/foxy/patreon/validator/ValidatorApplicationTests.java
+++ validator/src/test/java/com/foxy/patreon/validator/ValidatorApplicationTests.java
@@ -2,12 +2,22 @@ package com.foxy.patreon.validator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"amazon.dynamodb.endpoint=http://dynamodb-local:8000/",
+"amazon.aws.accesskey=test1",
+"amazon.aws.secretkey=test213",
+"amazon.aws.region=us-east-1"})
 class ValidatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testCreation(){
+
 	}
 
 }
