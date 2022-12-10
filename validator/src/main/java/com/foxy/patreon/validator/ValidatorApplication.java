e6129620650f2ebc780298eea9e36cb81b64a299
svrakiss <59843464+svrakiss@users.noreply.github.com>
2022-12-10 00:10:41 +0000

    added resttemplate

--- validator/src/main/java/com/foxy/patreon/validator/ValidatorApplication.java
+++ validator/src/main/java/com/foxy/patreon/validator/ValidatorApplication.java
@@ -2,12 +2,18 @@ package com.foxy.patreon.validator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ValidatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ValidatorApplication.class, args);
+	}
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 
 }
