a160abf42d1ab2f883fa45e8177c9281361f966f
svrakiss <59843464+svrakiss@users.noreply.github.com>
2022-12-13 07:33:00 +0000

    added tier and name. serialization is a go

--- validator/src/main/java/com/foxy/patreon/validator/service/ValidatorService.java
+++ validator/src/main/java/com/foxy/patreon/validator/service/ValidatorService.java
@@ -1,11 +1,16 @@
 package com.foxy.patreon.validator.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
+import com.foxy.patreon.validator.entity.PatronEntity;
+
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ValidatorService{
 
-    Mono<ResponseEntity<String>> updateMembers(String campaignId);
+    Mono<ResponseEntity<List<PatronEntity>>> updateMembers(String campaignId);
 
 }
--- validator/src/main/java/com/foxy/patreon/validator/service/ValidatorServiceImpl.java
+++ validator/src/main/java/com/foxy/patreon/validator/service/ValidatorServiceImpl.java
@@ -1,16 +1,22 @@
 package com.foxy.patreon.validator.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.foxy.patreon.validator.entity.PatronEntity;
 import com.foxy.patreon.validator.repository.PatronRepository;
+import com.nimbusds.jose.shaded.gson.JsonObject;
 
 import net.minidev.json.JSONObject;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service("validatorService")
@@ -21,21 +27,25 @@ public class ValidatorServiceImpl implements ValidatorService{
     @Autowired
     WebClient rest;
     @Override
-    public Mono<ResponseEntity<String>> updateMembers(String campaignId) {
+    public Mono<ResponseEntity<List<PatronEntity>>> updateMembers(String campaignId) {
         
         JSONObject reqJsonObject = new JSONObject();
         // need to explicitly state includes fields
         // includes entries have to be plural.
         // fields entries have to be singular
-        reqJsonObject.put("include",List.of("users"));
-        reqJsonObject.put("fields[user]",List.of("social_connections"));
-        reqJsonObject.put("fields[member]",List.of("patron_status"));
-        reqJsonObject.put("fields[tier]",List.of("title"));
-        var result=rest.get()
-        .uri("http://coolwhip_2:65010/campaign/members?campaign_id="+campaignId, reqJsonObject)
-        .accept(MediaType.APPLICATION_JSON)
+        reqJsonObject.put("include",List.of("currently_entitled_tiers","user"));
+        Map<String,List<String>> fields = new HashMap<>();
+        fields.put("user",List.of("social_connections"));
+        fields.put("member",List.of("patron_status"));
+        fields.put("tier",List.of("title"));
+        reqJsonObject.put("fields", fields);
+        var result=rest.post()
+        .uri("http://coolwhip_2:65010/campaign/members?campaign_id="+campaignId)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(reqJsonObject)
+         .accept(MediaType.APPLICATION_JSON)
         .retrieve()
-        .toEntity(String.class);
+        .toEntityList(PatronEntity.class);
         // System.out.println(result.block());
         return result;
     }
