51ef1cc004580564d457ffbaeab90c85816ea419
svrakiss <59843464+svrakiss@users.noreply.github.com>
2023-01-11 00:46:32 +0000

    cdk stack without spring config

--- infra/src/main/java/com/myorg/InfraApp.java
+++ infra/src/main/java/com/myorg/InfraApp.java
@@ -6,23 +6,26 @@ import software.amazon.awscdk.StackProps;
 
 import java.util.Arrays;
 
+import dev.stratospheric.cdk.ApplicationEnvironment;
+import dev.stratospheric.cdk.SpringBootApplicationStack;
+
 public class InfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new InfraStack(app, "InfraStack", StackProps.builder()
+        final InfraStack ifr=new InfraStack(app, "InfraStack", StackProps.builder()
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
 
                 // Uncomment the next block to specialize this stack for the AWS Account
                 // and Region that are implied by the current CLI configuration.
-                /*
+                
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
                         .region(System.getenv("CDK_DEFAULT_REGION"))
                         .build())
-                */
+                
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
@@ -35,7 +38,17 @@ public class InfraApp {
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                 .build());
-
+                String  environmentName = (String) app.getNode().tryGetContext("environmentName");
+                // requireNonEmpty(environmentName, "context variable 'environmentName' must not be null");
+                ApplicationEnvironment applicationEnvironment = new ApplicationEnvironment("Validator", environmentName);
+                SpringBootApplicationStack springBootApplicationStack = new SpringBootApplicationStack(app,
+                "SpringValidatorStack",
+                Environment.builder()
+                .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                .region(System.getenv("CDK_DEFAULT_REGION"))
+                .build(),
+                ifr.dockerImage.getImageUri());
+             
         app.synth();
     }
 }
--- infra/src/main/java/com/myorg/InfraStack.java
+++ infra/src/main/java/com/myorg/InfraStack.java
@@ -1,12 +1,28 @@
 package com.myorg;
 
 import software.constructs.Construct;
+
+import java.util.Collections;
+import java.util.List;
+
+import dev.stratospheric.cdk.ApplicationEnvironment;
+import dev.stratospheric.cdk.SpringBootApplicationStack;
+import software.amazon.awscdk.BundlingOptions;
+import software.amazon.awscdk.BundlingOutput;
+import software.amazon.awscdk.DockerBuildOptions;
+import software.amazon.awscdk.DockerImage;
+import software.amazon.awscdk.DockerVolume;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.codecommit.Code;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecr.assets.DockerImageAsset;
+import software.amazon.awscdk.services.lambda.Runtime;
 
 public class InfraStack extends Stack {
+    DockerImageAsset dockerImage;
     public InfraStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -20,5 +36,41 @@ public class InfraStack extends Stack {
         // final Queue queue = Queue.Builder.create(this, "InfraQueue")
         //         .visibilityTimeout(Duration.seconds(300))
         //         .build();
+
+        // List<String>  packagingInstructions = List.of(
+        //     "/bin/sh",
+        //     "-c",
+        //     "cd validator"+
+        //     "&& mvn -DskipTests clean install"+
+        //     "&& cp /asset-input/validator/target/validator.jar /asset-output/"
+        // );
+        // BundlingOptions options = BundlingOptions.builder()
+        // .command(packagingInstructions)
+        // .image(Runtime.JAVA_11.getBundlingImage())
+        // .volumes(
+        //     Collections.singletonList(
+        //         DockerVolume.builder()
+        //         .hostPath(System.getProperty("user.home")+"/.m2/")
+        //         .containerPath("/root/.m2/")
+        //         .build()
+        //     )
+        // ).user("root")
+        // .outputType(BundlingOutput.ARCHIVED)
+        // .entrypoint(List.of(
+        //     "java",
+        //     "-jar",
+        //     "validator.jar"
+        // ))
+        // .build();
+        
+        dockerImage = DockerImageAsset.Builder
+        .create(this, getStackName()+"Image")
+        .directory("..//validator//")
+        .build();
+        // Repository springRepo = Repository.Builder.create(this, getStackName()+"ECRRepo")
+        // .repositoryName(getStackName()+"ValidatorRepo")
+        // .build();
+        // System.out.println(dockerImage.getImage());
+        
     }
 }
